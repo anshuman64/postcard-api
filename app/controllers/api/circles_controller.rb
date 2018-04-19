27b8204456b96f8df049f1ca9310fd613a22cbf4
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-19 13:41:16 -0700

    Add delete circle

--- app/controllers/api/circles_controller.rb
+++ app/controllers/api/circles_controller.rb
@@ -18,6 +18,10 @@ class Api::CirclesController < ApplicationController
       render json: [error], status: 401 and return
     end
 
+    if params[:user_ids].size < 2
+      render json: ['Minimum 2 user_ids required'], status: 403 and return
+    end
+
     @circle = Circle.new({ creator_id: @client.id, name: params[:name] })
 
     if @circle.save
@@ -37,4 +41,25 @@ class Api::CirclesController < ApplicationController
       render json: @circle.errors.full_messages, status: 422
     end
   end
+
+  def destroy_circle
+    client, error = decode_token_and_find_user(request.headers['Authorization'])
+
+    if error
+      render json: [error], status: 401 and return
+    end
+
+    @circle = Circle.find(params[:id])
+
+    unless @circle
+      render json: ['Circle not found'], status: 404 and return
+    end
+
+    if @circle.destroy
+      render 'api/circles/show'
+    else
+      render json: @circle.errors.full_messages, status: 422
+    end
+  end
+
 end
