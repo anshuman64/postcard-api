c67119ed2baebb6dbe32506f20ee4bed570eea17
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-27 18:19:01 -0700

    Remove notification for post as message; change collapse_id to android_group

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < ActionController::API
     end
   end
 
+  # TODO: make this better for group messaging
   def create_notification(client_id, recipient_id, title, message, data)
     params = {
       app_id: ENV["ONE_SIGNAL_APP_ID"],
@@ -47,7 +48,7 @@ class ApplicationController < ActionController::API
       filters: [{"field": "tag", "key": "user_id", "relation": "=", "value": recipient_id.to_s}],
       data: data,
       headings: title,
-      collapse_id: client_id
+      android_group: client_id
     }
 
     begin
@@ -154,8 +155,6 @@ class ApplicationController < ActionController::API
   def get_message_notification_preview(message)
     if message.body
       message_preview = message.body
-    elsif message.post_id
-      message_preview = 'Clicked on your post!'
     else
       message_preview = 'Sent you an image.'
     end
